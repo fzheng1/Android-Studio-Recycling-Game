c211946fff0bf8d8c75f4730bfa3726b7deda386
fzheng1 <kaifenzheng@gmail.com>
2018-05-16 10:10:53 -0400

    made gameobjects abstract, added tick and render methods

--- app/src/main/java/com/strobertchs/enviro_game/Objects/GameObject.java
+++ app/src/main/java/com/strobertchs/enviro_game/Objects/GameObject.java
@@ -1,12 +1,25 @@
 package com.strobertchs.enviro_game.Objects;
 
-public class GameObject {
+public abstract class GameObject {
 
     protected int x, y;
 
     protected double velX, velY;
 
     protected ID id;
+
+    public GameObject(int x, int y, ID id){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+    }
+
+    // actions of the object each refresh
+    public abstract void tick();
+
+    // graphics of the object
+    public abstract void render();
+
 
 
 
