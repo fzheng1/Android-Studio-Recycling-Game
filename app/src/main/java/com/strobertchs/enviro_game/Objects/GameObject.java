01f05bb63553859a7516a14c0fcf123b801f5e96
fzheng1 <kaifenzheng@gmail.com>
2018-05-16 10:14:00 -0400

    added getters and setters as well as method to get the bounds of the objects

--- app/src/main/java/com/strobertchs/enviro_game/Objects/GameObject.java
+++ app/src/main/java/com/strobertchs/enviro_game/Objects/GameObject.java
@@ -1,5 +1,7 @@
 package com.strobertchs.enviro_game.Objects;
 
+import android.support.constraint.solver.widgets.Rectangle;
+
 public abstract class GameObject {
 
     protected int x, y;
@@ -20,8 +22,46 @@ public abstract class GameObject {
     // graphics of the object
     public abstract void render();
 
+    // outline of object to test for overlap
+    public abstract Rectangle getBounds();
 
+    public int getX() {
+        return x;
+    }
 
+    public void setX(int x) {
+        this.x = x;
+    }
 
+    public int getY() {
+        return y;
+    }
 
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public double getVelX() {
+        return velX;
+    }
+
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+
+    public double getVelY() {
+        return velY;
+    }
+
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
 }
