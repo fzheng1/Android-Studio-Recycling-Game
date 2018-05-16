c1c37da8e74ebcdc533e67bb5f2acbd5840b64f8
fzheng1 <kaifenzheng@gmail.com>
2018-05-16 10:34:24 -0400

    Added comments and tick method in handler

--- app/src/main/java/com/strobertchs/enviro_game/Handler.java
+++ app/src/main/java/com/strobertchs/enviro_game/Handler.java
@@ -1,5 +1,21 @@
 package com.strobertchs.enviro_game;
 
+import com.strobertchs.enviro_game.Objects.GameObject;
+
+import java.util.LinkedList;
+
 // this class will decide what the game does with the gameobjects
 public class Handler {
+
+    // where the objects that are created are going to be stored
+    //linked list is an expandable array
+    // <E> indicates what the list is storing
+    LinkedList<GameObject> gameObjects = new LinkedList<>();
+
+    // run the tick methods of the gameobjects
+    public void tick(){
+        for (GameObject tempobject: gameObjects) {
+            tempobject.tick();
+        }
+    }
 }
--- app/src/main/java/com/strobertchs/enviro_game/Objects/ID.java
+++ app/src/main/java/com/strobertchs/enviro_game/Objects/ID.java
@@ -1,5 +1,6 @@
 package com.strobertchs.enviro_game.Objects;
 
+// Possible IDs each object can be assigned
 enum ID {
 
     recyclable(),
