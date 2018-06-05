ffad54bb9ad0212ba78b4ffa1c6949062d47b318
SMinich <simon.minich18@ycdsbk12.ca>
2018-06-05 15:54:45 -0400

    Added new classes (representing waste objects)

---
+++ app/src/main/java/com/strobertchs/enviro_game/Objects/Styrofoam.java
@@ -0,0 +1,71 @@
+package com.strobertchs.enviro_game.Objects;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.support.constraint.solver.widgets.Rectangle;
+import android.view.MotionEvent;
+
+import com.strobertchs.enviro_game.Handler;
+import static com.strobertchs.enviro_game.MainThread.canvas;
+
+public class Styrofoam extends GameObject{
+
+    private Bitmap image;
+
+    public Styrofoam(int x, int y, ID id, Bitmap res){
+        super(x, y, id);
+        image = res;
+    }
+
+    public void update(){
+        x += velX;
+    }
+
+    public void draw(Canvas canvas){
+        canvas.drawBitmap(image, x, y, null);
+
+    }
+
+    public void dispose(){
+        image.recycle();
+    }
+
+
+    @Override
+    public Rectangle getBounds() {
+        this.rectangle.setBounds(x, y, 50, 100);
+        return this.rectangle;
+    }
+
+    public void setVector(int vector){
+        this.velX = vector;
+    }
+
+}
+
+
+
+//    public void setVector(int vector){
+//        this.dx = vector;
+//    }
+//    @Override
+//    public void update() {
+//        x += velX;
+//        if (x < -com.strobertchs.enviro_game.GamePanel.WIDTH){
+//             // TODO remove the object
+//        }
+//    }
+//
+//    // TODO update override if the object is touched by player
+//
+//    @Override
+//    public void draw(Canvas canvas) {
+//        canvas.drawBitmap(image, x, 0, null);
+//        if(x<0){
+//            // TODO Object is destroyed
+//        }
+//
+//    }
+
+
+
